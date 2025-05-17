0b70be07e77e3e5bd9ede002a838db9b6704ba80
OtariTB <tabatadzeoto50@gmail.com>
2025-05-17 21:03:40 +0400

    1.0

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Monitor Store</title>
+    <link rel="stylesheet" href="./styles/styles.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Products</li>
+                <li></li>
+            </ul>
+            <button>cart</button>
+        </nav>
+    </header>
+</body>
+</html>
