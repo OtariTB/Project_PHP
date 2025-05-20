bca2599b1567e29075f7acfd90220ad3b0e5de34
OtariTB <tabatadzeoto50@gmail.com>
2025-05-20 20:10:17 +0400

    1.2.1

--- index.php
+++ index.php
@@ -17,18 +17,34 @@ include_once './config/connect.php';
 </head>
 <body>
     <header>
+        <div class="container header-container">
+            <a href="./index.php" class="logo">MonitorStore</a>
+            <div class="search-bar">
+                <input type="text" placeholder="Search monitors... 🔍">
+            </div>
+            <div class="nav-icons">
+                <div class="nav-icon">👤</div>
+                <div class="nav-icon">❤️</div>
+                <div class="nav-icon">🛒</div>
+            </div>
+        </div>
         <nav class="navbar">
             <ul>
                 <li><a href="./index.php">Home</a></li>
                 <li><a href="./includes/products.php">Products</a></li>
             </ul>
-            <button>cart</button>
+            <button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+            </svg>
+            </button>
 
             <ul>
-                <li><a href="./includes/login.php">login</a></li>
-                <li><a href="./includes/register.php">register</a></li>
+                <li><a href="./includes/login.php"> Log In</a></li>
+                <li><a href="./includes/register.php">Register</a></li>
             </ul>
         </nav>
+
+        
     </header>
 </body>
 </html>
